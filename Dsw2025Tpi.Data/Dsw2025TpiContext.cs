1eeba9ebd64307118d842f5986879ed3c93923dd
Lautaro Castillo <lautaro.castillo@alu.frt.utn.edu.ar>
2025-11-09 04:41:48 -0300

    Se agregó AuthenticateContext

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -1,7 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Data.Configurations;
+using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2025Tpi.Data;
 
-public class Dsw2025TpiContext: DbContext
+public class Dsw2025TpiContext : DbContext
 {
+    public Dsw2025TpiContext(DbContextOptions<Dsw2025TpiContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new CustomerConfiguration());
+        builder.ApplyConfiguration(new ProductConfiguration());
+        builder.ApplyConfiguration(new OrderConfiguration());
+        builder.ApplyConfiguration(new OrderItemConfiguration());
+    }
 }
---
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Dsw2025Tpi.Domain.Entities;
+
+public class Customer : EntityBase
+{
+    public string? Email { get; set; }
+    public string? Name { get; set; }
+    public string? PhoneNumber { get; set; }
+
+    public Customer() { }
+}
