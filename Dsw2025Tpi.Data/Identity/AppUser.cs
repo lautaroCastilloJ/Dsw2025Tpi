821e008188367617ca934798da5806f414e53156
Lautaro Castillo <lautaro.castillo@alu.frt.utn.edu.ar>
2025-11-09 04:08:34 -0300

    Se agregaron clases auxiliares que heredan propiedades de Identity

---
+++ Dsw2025Tpi.Data/Identity/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Dsw2025Tpi.Data.Identity;
+
+public class AppUser : IdentityUser 
+{
+    public string? DisplayName { get; set; }
+    public Guid CustomerId { get; set; }
+
+
+
+}
