378c63d3a842029aec1af2f8498c637170fd7973
Lautaro Castillo <lautaro.castillo@alu.frt.utn.edu.ar>
2025-11-14 18:24:44 -0300

    Refactorizando

---
+++ Dsw2025Tpi.Application/Dtos/Product/ProductRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Dtos.Product;
+
+public record ProductRequest(
+    string Sku,
+    string InternalCode,
+    string Name,
+    string? Description,
+    decimal CurrentUnitPrice,
+    int StockQuantity
+);
