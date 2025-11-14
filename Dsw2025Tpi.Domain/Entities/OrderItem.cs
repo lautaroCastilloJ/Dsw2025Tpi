d18fc35713371a1dbff48cf8e92f825d4e4b4e32
Lautaro Castillo <lautaro.castillo@alu.frt.utn.edu.ar>
2025-11-14 01:19:56 -0300

    actualizando dominio

--- Dsw2025Tpi.Domain/Entities/OrderItem.cs
+++ Dsw2025Tpi.Domain/Entities/OrderItem.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dsw2025Tpi.Domain.Entities
+namespace Dsw2025Tpi.Domain.Entities;
+
+public class OrderItem
 {
-    internal class OrderItem
-    {
-    }
 }
